855e9d1d695b8ed3fecbe0b4607dd3c99267b84e
EFLLP <erickllanos30@gmail.com>
2019-02-22 00:58:02 -0600

    Change of the UI
    *.- I used navigation drawer.
    *.- The icon o the hamburguer is pending, now is missing.

--- BlackList/BlackList/MainActivity.cs
+++ BlackList/BlackList/MainActivity.cs
@@ -5,17 +5,46 @@ using Android.Telephony;
 using Android.Content;
 using Android;
 using Android.Content.PM;
+using Android.Support.V7.App;
+using Android.Support.V4.Widget;
+using Android.Views;
+using Android.Support.Design.Widget;
+using V7Toolbar = Android.Support.V7.Widget.Toolbar;
+
+
 
 namespace BlackList
 {
     [Activity(Label = "BlackList", MainLauncher = true, Icon = "@mipmap/icon")]
-    public class MainActivity : Activity
+    public class MainActivity : AppCompatActivity
     {
-      
+
+
+        DrawerLayout drawerLayout;
+        NavigationView navigationView;
+     
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
+
+            var toolbar = FindViewById<V7Toolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(toolbar);
+
+            SupportActionBar.SetDisplayHomeAsUpEnabled(true);
+            SupportActionBar.SetDisplayShowTitleEnabled(false);
+            SupportActionBar.SetHomeAsUpIndicator(Resource.Drawable.abc_ic_star_black_36dp);
+            SupportActionBar.SetHomeButtonEnabled(true);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+            drawerLayout = FindViewById<DrawerLayout>(Resource.Id.drawer_layout);
+
+            navigationView = FindViewById<NavigationView>(Resource.Id.nav_view);
+            if (navigationView != null)
+                setupDrawerContent(navigationView);
+
             int Request = 0;
             if ((int)Build.VERSION.SdkInt >= 23)
             {
@@ -35,6 +64,24 @@ namespace BlackList
                 Toast.MakeText(this, "Permiso denegado", ToastLength.Short)
                 .Show();
 
+        }
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            switch (item.ItemId)
+            {
+                case Android.Resource.Id.Home:
+                    drawerLayout.OpenDrawer(Android.Support.V4.View.GravityCompat.Start);
+                    return true;
+            }
+
+            return base.OnOptionsItemSelected(item);
+        }
+        void setupDrawerContent(NavigationView navigationView)
+        {
+            navigationView.NavigationItemSelected += (sender, e) => {
+                e.MenuItem.SetChecked(true);
+                drawerLayout.CloseDrawers();
+            };
         }
 
 
